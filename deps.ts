cb7fa11d0675808b3b6bfbb9500d5aebad033f45
rymiyamoto <ry.miyamoto129@gmail.com>
2023-06-03 02:53:19 +0900

    make deps

---
+++ deps.ts
@@ -0,0 +1,6 @@
+// hot to use: import { assertEquals, runTests, test } from "./deps.ts";
+export {
+  assert,
+  assertEquals,
+  assertStrContains,
+} from "https://deno.land/std@0.94.0/testing/asserts.ts";
